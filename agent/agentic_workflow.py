18d9d95e88f2cae49557a4d6910e32654f63b214
akash <akash.karma24@gmail.com>
2026-09-21 22:03:39 +0530

    code

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,23 @@
+from utils.model_loader import ModelLoader
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.calculator_tool import CalculatorTool
+from tools.currency_conversion_tool import CurrencyConvertorTool
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+
+class GraphBuiler():
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __call__(self):
+        pass
